38e903b2966fb99e1027148d0db53e9cf3f2a609
7flying <diez.irene@opendeusto.es>
2014-04-07 23:16:40 +0200

    Coordinates + draw function, needs optimization

--- trinket_tic_tac_toe/trinket_tic_tac_toe.ino
+++ trinket_tic_tac_toe/trinket_tic_tac_toe.ino
@@ -4,6 +4,7 @@
 
 Adafruit_BicolorMatrix m = Adafruit_BicolorMatrix();
 char turn = 'x';
+int puntx, punty;
 char pos[] = {'n','n','n','n','n','n','n','n','n'};
 static const uint8_t PROGMEM
   board[] = {
@@ -16,6 +17,11 @@ static const uint8_t PROGMEM
     B00100100,
     B00100100,
   };
+int coor[][2] PROGMEM = {
+ {0, 0}, {4,0}, {6,0},
+ {24,3}, {27,3}, {30,3},
+ {48,6}, {51,6}, {54,6}
+ };
 void setup() {
  m.begin(0x70);
  pinMode(1,INPUT);
@@ -24,14 +30,21 @@ void setup() {
  digitalWrite(3, HIGH);
 }
 void loop() {
+  
 }
+
+void movePointer(){
+
+}
+// Draw 'x' or 'o'
 void draw(){
   int i = 0;
   for(i=0; i<9; i++){
     if(pos[i] != 'n'){
       if(pos[i] == 'x'){
-        
+        m.drawLine(coor[i][0],coor[i][1],1,1, LED_GREEN);
       }else{
+		m.drawRect(coor[i][0],coor[i][1],1,1, LED_RED);
       }
     }
   }
